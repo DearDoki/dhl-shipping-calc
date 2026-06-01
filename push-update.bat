62fa6eb140a993961a8c96762503c332eafb8a7b
DearDoki <DearDoki@users.noreply.github.com>
2026-06-01 10:08:53 +0800

    fix: fuel surcharge 2 decimal places 48.75

--- push-update.bat
+++ push-update.bat
@@ -2,5 +2,5 @@
 set http_proxy=http://127.0.0.1:10809
 set https_proxy=http://127.0.0.1:10809
 git add .
-git commit -m "fix: fuel surcharge display 2 decimal places"
+git commit -m "fix: fuel surcharge 2 decimal places 48.75%"
 git push origin main
